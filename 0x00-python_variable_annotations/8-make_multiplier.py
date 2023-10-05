585418a21ade4921b4e6ea15d96919da85558ce7
Endesew Bantayehu <117736159+EndesewB@users.noreply.github.com>
2023-10-05 14:47:53 +0300

    Update 8-make_multiplier.py

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
+"""multiplier"""
 from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
     Takes a float multiplier and returns a function that multiplies a float by multiplier.
-
-    :param multiplier: A float to be used for multiplication.
-    :return: A function that multiplies a float by the specified multiplier.
     """
     def multiplier_function(x: float) -> float:
         """
         Multiplies a float by the specified multiplier.
-
-        :param x: The float to be multiplied.
-        :return: The result of the multiplication as a float.
         """
         return x * multiplier
 
