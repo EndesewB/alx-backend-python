d1be1a394931cf1fa795cca73b36310f76dc9440
Endesew Bantayehu <117736159+EndesewB@users.noreply.github.com>
2023-10-05 14:59:09 +0300

    Update 100-safe_first_element.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
+"""returns first element of the sequence"""
 from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence) -> Union[Any, None]:
     """
     Takes a sequence and returns its first element if it exists, or None if the sequence is empty.
-
-    :param lst: A sequence of elements of unknown type.
-    :return: The first element of the sequence or None if the sequence is empty.
     """
     if lst:
         return lst[0]
