97a095406fb65be0615d502d4f60f0e01e60813a
Endesew Bantayehu <117736159+EndesewB@users.noreply.github.com>
2023-10-05 11:21:53 +0300

    Create 9-element_length.py

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,11 @@
+from typing import Iterable, Sequence, List, Tuple
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Takes a list of sequences and returns a list of tuples,
+    where each tuple contains an element from the input list and its length.
+
+    :param lst: An iterable of sequences.
+    :return: A list of tuples, each containing a sequence from the input and its length.
+    """
+    return [(i, len(i)) for i in lst]
