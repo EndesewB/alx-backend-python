584b6a8b2b66e04ae5c15018eaa30fd275d55512
Endesew Bantayehu <117736159+EndesewB@users.noreply.github.com>
2023-10-05 11:10:46 +0300

    Create 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def to_str(n: float) -> str:
+    """
+    Converts a floating-point number to its string representation.
+
+    :param n: The floating-point number.
+    :return: The string representation of the input number.
+    """
+    return str(n)
