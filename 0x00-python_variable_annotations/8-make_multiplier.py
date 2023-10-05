c9a19fe19b6bb60b8db45cc547e0f2719c39ee3e
Endesew Bantayehu <117736159+EndesewB@users.noreply.github.com>
2023-10-05 14:49:28 +0300

    Update 8-make_multiplier.py

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -6,9 +6,7 @@ from typing import Callable
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
     Takes a float multiplier and returns a function that multiplies a float by multiplier.
-    """
-
-    
+    """ 
     def multiplier_function(x: float) -> float:
         """
         Multiplies a float by the specified multiplier.
