2d22bfd2c95153c4273f402f9625c85745faa840
Endesew Bantayehu <117736159+EndesewB@users.noreply.github.com>
2023-10-05 15:02:55 +0300

    Update 100-safe_first_element.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """returns first element of the sequence"""
-from typing import Sequence, Any, Union
+
+import typing
 
 
-def safe_first_element(lst: Sequence) -> Union[Any, None]:
-    """
-    Takes a sequence and returns its first element if it exists, or None if the sequence is empty.
-    """
+def safe_first_element(lst: typing.Sequence[typing.Any]) -> \
+        typing.Union[typing.Any, None]:
+    """Duck-typed annotation"""
     if lst:
         return lst[0]
     else:
