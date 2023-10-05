480a54d175da263bad59f611803ca1a42370f706
Endesew Bantayehu <117736159+EndesewB@users.noreply.github.com>
2023-10-05 11:08:57 +0300

    Create 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import math
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a given floating-point number.
+
+    :param n: The floating-point number.
+    :return: The floor of the input number as an integer.
+    """
+    return math.floor(n)
