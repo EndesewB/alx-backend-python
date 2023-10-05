bb39d4d30de677f9f034fb6f4acaa9f1481a830a
Endesew Bantayehu <117736159+EndesewB@users.noreply.github.com>
2023-10-05 10:54:08 +0300

    add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floating-point numbers and returns their sum.
+
+    :param a: The first floating-point number.
+    :param b: The second floating-point number.
+    :return: The sum of a and b as a floating-point number.
+    """
+    return a + b
