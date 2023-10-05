25619849de44367c0b2e650b83e138b6bcf4d9e4
Endesew Bantayehu <117736159+EndesewB@users.noreply.github.com>
2023-10-05 14:46:33 +0300

    Update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
-""" returns a tuble from string, int or float""
+""" returns a tuble from string, int or float"""
 
-import typing 
+import typing
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """
-    Takes a string k and an int or float v and returns a tuple.
-    The first element of the tuple is the string k.
-    The second element is the square of the int/float v and is annotated as a float.
-    """
+def to_kv(k: str, v: typing.Union[int, float]) -> typing.Tuple[str, float]:
+    """returns a tuple of the string & square of v as float"""
     return (k, float(v * v))
