78cb2814252e0d9fd17aa6e22e2446948a7c6052
Endesew Bantayehu <117736159+EndesewB@users.noreply.github.com>
2023-10-05 14:52:14 +0300

    Update 8-make_multiplier.py

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
-"""multiplier"""
-from typing import Callable
+"""
+multiplier
+"""
+
+
+import typing
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
