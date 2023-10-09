d59a65be4b1561d466ab0c02a65acb23845dddfb
Endesew Bantayehu <117736159+EndesewB@users.noreply.github.com>
2023-10-09 21:16:10 +0300

    basic_async_syntax

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"" 
+"""
 async
 """
 
@@ -13,6 +13,6 @@ async def wait_random(max_delay: int = 10) -> float:
     Asynchronous coroutine that waits for a random delay between 0 and max_delay
     and eventually returns it.
     """
-    delay = random.uniform() * max_delay
+    delay = random.random() * max_delay
     await asyncio.sleep(delay)
     return delay
